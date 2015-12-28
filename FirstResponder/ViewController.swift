81cd8420219198295ffd7c56ab86f5991c399bde
Michael Stroh <schleppi@michael-mbp.fritz.box>
2015-12-28 13:37:50 +0100

    FirstResponder lauffähig

--- FirstResponder/ViewController.swift
+++ FirstResponder/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var prenameTextfield: UITextField!
+    @IBOutlet weak var lastnameTextfield: UITextField!
+    @IBOutlet weak var fullnameLabel: UILabel!
+    
+    var firstname = ""
+    var lastname = ""
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == prenameTextfield {
+            firstname = textField.text!
+            fullnameLabel.text = firstname
+            lastnameTextfield.becomeFirstResponder()
+        } else {
+            lastname = textField.text!
+            fullnameLabel.text = firstname + " " + lastname
+            lastnameTextfield.resignFirstResponder()
+        }
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.prenameTextfield.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
